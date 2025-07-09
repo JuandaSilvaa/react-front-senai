c6b085dcdd1109ebaef924fc9b8fef72410e843f
JuandaSilvaa <almeidajuan529@gmail.com>
2025-07-09 20:25:47 -0300

    .

--- wishlist/src/routes/Router.jsx
+++ wishlist/src/routes/Router.jsx
@@ -1,12 +1,20 @@
 import { Route, Routes } from "react-router";
 import App from "../App";
 import { Layout } from "../components/Layout/Layout";
+import { useCallback, useState } from "react";
 
 export const Router = () => {
+    const [search, setSearch] = useState("");
+    const onSearch = useCallback((searchValue) => {
+        setSearch(searchValue);
+      });
+      const onClear = useCallback(() => {
+        setSearch("");
+      });
   return (
     <Routes>
-      <Route element={<Layout />}>
-        <Route path="/wishlist" element={<App />} />
+      <Route element={<Layout onClear={onClear} onSearch={onSearch}/>}>
+        <Route path="/wishlist" element={<App search={search} />} />
       </Route>
     </Routes>
   );
